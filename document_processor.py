d4cb0eb4d2ed3cde0c22c3b6378ed0f31afaa064
Johnson Joseph <117355554+Johnson-ML-Engineer@users.noreply.github.com>
2024-08-01 15:08:14 +0530

    document_processor.py

--- document_processor.py
+++ document_processor.py
@@ -1,8 +1,8 @@
-from langchain.document_loaders import PyPDFDirectoryLoader
+from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 def read_documents(directory):
-    return PyPDFDirectoryLoader(directory).load()
+    return PyPDFLoader(directory).load()
 
 def chunk_data(docs, chunk_size=800, chunk_overlap=40):
     return RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap).split_documents(docs)
